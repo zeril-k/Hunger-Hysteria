2ac68646e6f8cc0854ae37b6bfcf276f5214eb7a
zeril_ <zerilstanford@gmail.com>
2025-09-28 14:39:44 -0500

    Scientists added

--- Assets/Scripts/Player/lives.cs
+++ Assets/Scripts/Player/lives.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class lives : MonoBehaviour
 {
-    public int Lives;
+    public int Lives = 3;
     public int maxLives = 3;
 
     public AudioSource audioSource;
     audioManager audioManager;
+
+    public int livesCount;
+    public Text livesText;
 
     private void Awake()
     {
@@ -22,7 +26,7 @@ public class lives : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        livesText.text =livesCount.ToString();
     }
 
     public void TakeDamage(int amount)
