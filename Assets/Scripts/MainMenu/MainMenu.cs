6f9ca88a49705c3753e4152f60c3e7ecee13205c
E1it3Dr3am3r <zamckenna05@gmail.com>
2025-09-28 14:09:43 -0500

    play and quit

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+   
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
